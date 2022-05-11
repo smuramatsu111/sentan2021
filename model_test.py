1b2486a7dc249d1ccd41d215c262884478a17fd3
smuramatsu111 <smuramatsu@kanolab.net>
2022-05-11 14:27:22 +0900

    検証用のプログラム

--- model_test.py
+++ model_test.py
@@ -4,5 +4,5 @@ import sentencepiece as spm
 sp = spm.SentencePieceProcessor()
 sp.Load("sentencepiece.model")
 
-for i in sp.EncodeAsIds("セネガル勝ってるよ"):
+for i in sp.EncodeAsIds("織田信長は武将である。"):
     print(i, sp.IdToPiece(i))
